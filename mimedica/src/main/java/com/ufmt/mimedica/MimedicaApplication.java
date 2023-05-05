1a83900e428e6dc1367854daa1d1caf4d2026354
RodrigoCelso <rodrigo.ca.bonfim@gmail.com>
2023-05-04 21:14:56 -0400

    Commit inicial Commit inicial com o gitignore e os arquivos com as dependencias

---
+++ mimedica/src/main/java/com/ufmt/mimedica/MimedicaApplication.java
@@ -0,0 +1,13 @@
+package com.ufmt.mimedica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimedicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MimedicaApplication.class, args);
+	}
+
+}
---
+++ mimedica/src/test/java/com/ufmt/mimedica/MimedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufmt.mimedica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
