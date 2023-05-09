e64d05f72bbfa1bc602668fe691f26aa6e99800c
RodrigoCelso <rodrigo.ca.bonfim@gmail.com>
2023-05-09 02:02:46 -0400

    Cria as relações da entidade Hospital Cria as relações da entidade Hospital com HospitalMedico

--- mimedica/src/main/java/com/ufmt/mimedica/hospital/Hospital.java
+++ mimedica/src/main/java/com/ufmt/mimedica/hospital/Hospital.java
@@ -1,11 +1,14 @@
 package com.ufmt.mimedica.hospital;
 
+import java.util.List;
+
 import javax.annotation.Generated;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -22,4 +25,6 @@ public class Hospital {
     private String nome;
     @Column
     private String cnpj;
+    @OneToMany(mappedBy = "medicoId")
+    private List<HospitalMedico> hospitalMedicos;
 }
