11b70134577f99f5bace3bea253dfd9023461f3a
RodrigoCelso <rodrigo.ca.bonfim@gmail.com>
2023-05-09 01:25:52 -0400

    Cria a entidade especialidade Cria a entidade especialidade e seus atributos

---
+++ mimedica/src/main/java/com/ufmt/mimedica/especialidade/Especialidade.java
@@ -0,0 +1,23 @@
+package com.ufmt.mimedica.especialidade;
+
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "especialidade")
+@SequenceGenerator(name = "seqEspecialidade", sequenceName = "seq_especialidade_id", allocationSize = 1)
+@lombok.Getter
+@lombok.Setter
+public class Especialidade {
+    @Id
+    @GeneratedValue(generator = "seqEspecialidade", strategy = GenerationType.SEQUENCE)
+    private int id;
+    @Column
+    private String tipo;
+}
