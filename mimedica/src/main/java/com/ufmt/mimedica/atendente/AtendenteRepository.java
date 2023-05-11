059e6d222d481e3aa88320e472099ff4306a6bd5
RodrigoCelso <rodrigo.ca.bonfim@gmail.com>
2023-05-11 19:12:11 -0400

    Cria o repository da entidade Atendente Cria o repository da entidade Atendente.

---
+++ mimedica/src/main/java/com/ufmt/mimedica/atendente/AtendenteRepository.java
@@ -0,0 +1,8 @@
+package com.ufmt.mimedica.atendente;
+
+import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+
+public interface AtendenteRepository 
+    extends JpaRepositoryImplementation<Atendente, Integer>{
+    
+}
